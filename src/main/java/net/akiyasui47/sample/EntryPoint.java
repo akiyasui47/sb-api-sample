2176c8a276e936784b768773dce5bb1e91398c95
Akihiko Yasui <aki47dev@gmail.com>
2018-01-13 18:14:24 +0900

    Hello World!

--- src/main/java/net/akiyasui47/sample/EntryPoint.java
+++ src/main/java/net/akiyasui47/sample/EntryPoint.java
@@ -1,9 +1,11 @@
 package net.akiyasui47.sample;
 
-import org.springframework.boot.*;
-import org.springframework.boot.autoconfigure.*;
-import org.springframework.stereotype.*;
-import org.springframework.web.bind.annotation.*;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
  * EntryPoint
